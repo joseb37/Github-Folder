ad13f691c2ce81260379aef13434f88d159c9d31
joseb37 <josebenjamin73@gmail.com>
2017-04-01 14:32:11 -0400

    Added my super cool hello world code and super cool test program.

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+package hello.world;
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello world.\n");
+    }
+    
+}
---
+++ Test.java
@@ -0,0 +1,10 @@
+package test;
+import java.util.Scanner;
+public class Test {
+    public static void main(String[] args) {
+    Scanner myScanner = new Scanner(System.in);
+    int intFive = myScanner.nextInt();
+    System.out.print(intFive+" is a integer\n");
+    }
+    
+}
